aa6891ef1645fc4e3942562546c9abb198ec362c
Drew Holtzapple <dholtzap@ycp.edu>
2013-02-15 09:17:45 -0500

    added IDatabase interface
    the interface that can be used to define all of the controller class
    methods

---
+++ ycp-cs320-project/src/edu/ycp/CS320/jjda/IDatabase.java
@@ -0,0 +1,8 @@
+package edu.ycp.CS320.jjda;
+
+//interface for the controller classes to implement. The methods can be defined in the 
+//actual controller classes then after they implement this interface.
+public interface IDatabase {
+	public void addToDatabase();
+	public String getFromDatabase();
+}
