ed31f58ba44b1bf34993c84837f91ec135a8e9ec
Alana Palmerini <apalmeri@ycp.edu>
2013-02-20 12:03:20 -0500

    initial GWT project for user interface

---
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/ui/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package edu.ycp.CS320.ui.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
