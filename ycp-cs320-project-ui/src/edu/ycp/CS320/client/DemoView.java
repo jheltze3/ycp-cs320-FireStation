203ad172a9adeccb3fd6b7ce3369245187f5bf91
Alana Palmerini <apalmeri@ycp.edu>
2013-02-22 13:33:39 -0500

    Fixed login page

---
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/client/DemoView.java
@@ -0,0 +1,61 @@
+package edu.ycp.CS320.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.i18n.client.HasDirection.Direction;
+import com.google.gwt.user.client.ui.TextBoxBase;
+import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.Button;
+
+public class DemoView extends Composite {
+	public DemoView() {
+		
+		LayoutPanel layoutPanel = new LayoutPanel();
+		layoutPanel.setStyleName("fireStation-panelBackground");
+		initWidget(layoutPanel);
+		
+		Label lblWelcomeToYork = new Label("Welcome To York Fire Station");
+		lblWelcomeToYork.setStyleName("Title");
+		lblWelcomeToYork.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		layoutPanel.add(lblWelcomeToYork);
+		layoutPanel.setWidgetLeftWidth(lblWelcomeToYork, 333.0, Unit.PX, 193.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(lblWelcomeToYork, 17.0, Unit.PX, 25.0, Unit.PX);
+		
+		Label lblUsername = new Label("Username:");
+		layoutPanel.add(lblUsername);
+		layoutPanel.setWidgetLeftWidth(lblUsername, 269.0, Unit.PX, 62.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(lblUsername, 106.0, Unit.PX, 18.0, Unit.PX);
+		
+		InlineLabel nlnlblPassword = new InlineLabel("Password:");
+		layoutPanel.add(nlnlblPassword);
+		layoutPanel.setWidgetLeftWidth(nlnlblPassword, 269.0, Unit.PX, 66.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(nlnlblPassword, 141.0, Unit.PX, 18.0, Unit.PX);
+		
+		TextBox textBox = new TextBox();
+		layoutPanel.add(textBox);
+		layoutPanel.setWidgetLeftWidth(textBox, 341.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(textBox, 96.0, Unit.PX, 34.0, Unit.PX);
+		
+		PasswordTextBox passwordTextBox = new PasswordTextBox();
+		layoutPanel.add(passwordTextBox);
+		layoutPanel.setWidgetLeftWidth(passwordTextBox, 341.0, Unit.PX, 173.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(passwordTextBox, 136.0, Unit.PX, 34.0, Unit.PX);
+		
+		Button btnLogIn = new Button("Log In");
+		layoutPanel.add(btnLogIn);
+		layoutPanel.setWidgetLeftWidth(btnLogIn, 397.0, Unit.PX, 56.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(btnLogIn, 176.0, Unit.PX, 25.0, Unit.PX);
+		
+		Button btnNewUser = new Button("New User?");
+		layoutPanel.add(btnNewUser);
+		layoutPanel.setWidgetLeftWidth(btnNewUser, 383.0, Unit.PX, 81.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(btnNewUser, 207.0, Unit.PX, 25.0, Unit.PX);
+	}
+}
+
--- ycp-cs320-project-ui/src/edu/ycp/CS320/client/Ycp_cs320_project_ui.java
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/client/Ycp_cs320_project_ui.java
@@ -1,6 +1,9 @@
 package edu.ycp.CS320.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -10,5 +13,13 @@ public class Ycp_cs320_project_ui implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		LayoutPanel panel = new LayoutPanel();
+		
+		DemoView demoView = new DemoView();
+		panel.add(demoView);
+		
+		RootLayoutPanel.get().add(panel);
+		RootLayoutPanel.get().setWidgetLeftRight(panel, 0.0, Unit.PX, 0.0, Unit.PX);
+		RootLayoutPanel.get().setWidgetTopBottom(panel, 0.0, Unit.PX, 0.0, Unit.PX);
 	}
 }
