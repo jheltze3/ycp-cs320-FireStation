10555335f285dee48e1aa80d9c94edfcaa17aca9
Joshua Heltzel <jheltze3@ycp.edu>
2013-04-05 09:59:32 -0400

    Added scroll panel details

--- ycp-cs320-project-controllers/src/edu/ycp/cs320/controllers/GetApparatusController.java
+++ ycp-cs320-project-controllers/src/edu/ycp/cs320/controllers/GetApparatusController.java
@@ -23,6 +23,18 @@ public class GetApparatusController implements Serializable{
 		fireApparatus.getFireApparatusSpec().setType("Pumper-Rescue");
 		db.addFireApparatusToDB(fireApparatus);
 		
+		for(int i=0; i<4; i++){
+			FireApparatus fireApparatus1 = new FireApparatus();
+			fireApparatus1.getFireApparatusSpec().setName("Engine "+Integer.toString(i));
+			
+			fireApparatus.getFireApparatusSpec().setDescription("A Fire Engine");
+			fireApparatus.getFireApparatusSpec().setMake("Pierce");
+			fireApparatus.getFireApparatusSpec().setModel("Arrow XT PUC");
+			fireApparatus.getFireApparatusSpec().setYear("2009");
+			fireApparatus.getFireApparatusSpec().setType("Pumper-Rescue");
+			db.addFireApparatusToDB(fireApparatus1);
+		}
+		
 		return db.getFireApparatusFromDB();
 	}
 }
