b55929c518dcad0fb30ce2a79e8007191cd8b06d
Drew Holtzapple <dholtzap@ycp.edu>
2013-04-05 13:22:04 -0400

    updated FormView

--- ycp-cs320-project-ui/src/edu/ycp/CS320/client/FormView.java
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/client/FormView.java
@@ -16,9 +16,9 @@ import com.google.gwt.dom.client.Style.Unit;
 
 public class FormView extends Composite {
 	private TextBox nameTextBox;	
-	private TextBox pwTextBox;
+	private TextBox cellTextBox;
 	private Label nameLabel;
-	private Label pwLabel;
+	private Label cellLabel;
 	
 	/**
 	 * TODO: MODIFY DEMO VIEW ADD NEW USER BUTTON TO REDIRECT TO A FORM PAGE WHERE THEY CAN FILL OUT THEIR INFORMATION
@@ -41,17 +41,19 @@ public class FormView extends Composite {
 		layout.setWidgetLeftWidth(nameTextBox, 86.0, Unit.PX, 173.0, Unit.PX);
 		layout.setWidgetTopHeight(nameTextBox, 49.0, Unit.PX, 34.0, Unit.PX);
 		
-		pwLabel = new Label();
-		pwLabel.setText("Password: ");
-		layout.add(pwLabel);
-		layout.setWidgetLeftWidth(pwLabel, 86.0, Unit.PX, 173.0, Unit.PX);
-		layout.setWidgetTopHeight(pwLabel, 90.0, Unit.PX, 24.0, Unit.PX);
+		cellLabel = new Label();
+		cellLabel.setText("Cell Phone: ");
+		layout.add(cellLabel);
+		layout.setWidgetLeftWidth(cellLabel, 86.0, Unit.PX, 173.0, Unit.PX);
+		layout.setWidgetTopHeight(cellLabel, 90.0, Unit.PX, 24.0, Unit.PX);
 		
-		pwTextBox = new TextBox();
-		pwTextBox.setText("");
-		layout.add(pwTextBox);
-		layout.setWidgetLeftWidth(pwTextBox, 86.0, Unit.PX, 173.0, Unit.PX);
-		layout.setWidgetTopHeight(pwTextBox, 117.0, Unit.PX, 34.0, Unit.PX);
+		cellTextBox = new TextBox();
+		cellTextBox.setText("");
+		layout.add(cellTextBox);
+		layout.setWidgetLeftWidth(cellTextBox, 86.0, Unit.PX, 173.0, Unit.PX);
+		layout.setWidgetTopHeight(cellTextBox, 117.0, Unit.PX, 34.0, Unit.PX);
+		
+		//TODO: create contact info from textbox info
 		
 		
 	}
