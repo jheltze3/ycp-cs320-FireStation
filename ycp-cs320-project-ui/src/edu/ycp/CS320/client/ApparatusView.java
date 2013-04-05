0c665d4f18d5aabaa94944aa56ed93f992860571
Joshua Heltzel <jheltze3@ycp.edu>
2013-04-05 13:34:12 -0400

    Home page changes added

--- ycp-cs320-project-ui/src/edu/ycp/CS320/client/ApparatusView.java
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/client/ApparatusView.java
@@ -26,13 +26,15 @@ public class ApparatusView extends Composite implements ISubscriber {
 	private Label lblEquipmentHeader;
 	private LayoutPanel layoutPanel;
 	private Label lblName;
+	private Label lblName01;
 	private Label lblTitle;
-	private FireApparatusList fireApparatusList;
+	private ArrayList<FireApparatus> fireApparatusList;
 	private Label lblType;
 	private Label lblModel;
 	private Label lblMake;
 	private Label lblmake_1;
 	private Label lblName_1;
+	private Label lblName_2;
 	private Label lblmodel_1;
 	private Label lbltype_1;
 	private Label lbldescription_1;
@@ -119,56 +121,48 @@ public class ApparatusView extends Composite implements ISubscriber {
 		layoutPanel.add(lblYear_1);
 		layoutPanel.setWidgetLeftWidth(lblYear_1, 107.0, Unit.PX, 56.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(lblYear_1, 122.0, Unit.PX, 18.0, Unit.PX);
-		
-		ScrollPanel scrollPanel = new ScrollPanel();
-		layoutPanel.add(scrollPanel);
-		layoutPanel.setWidgetLeftWidth(scrollPanel, 20.0, Unit.PX, 383.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(scrollPanel, 392.0, Unit.PX, 96.0, Unit.PX);
-		
-		FlowPanel flowPanel = new FlowPanel();
-		scrollPanel.setWidget(flowPanel);
-		flowPanel.setSize("100%", "100%");
-		
-		FlowPanel flowPanel_1 = new FlowPanel();
-		flowPanel.add(flowPanel_1);
-		
-		Label lblPanel = new Label("panel 1");
-		flowPanel_1.add(lblPanel);
-		
-		Button btnButton_2 = new Button("Button 1");
-		flowPanel_1.add(btnButton_2);
-		
-		FlowPanel flowPanel_2 = new FlowPanel();
-		flowPanel.add(flowPanel_2);
-		
-		Label lblPanel_1 = new Label("panel 2");
-		flowPanel_2.add(lblPanel_1);
-		
-		Button btnButton_1 = new Button("Button 2");
-		flowPanel_2.add(btnButton_1);
-		
-		FlowPanel flowPanel_3 = new FlowPanel();
-		flowPanel.add(flowPanel_3);
-		
-		Label lblNewLabel = new Label("panel 2");
-		flowPanel_3.add(lblNewLabel);
-		
-		Button btnButton = new Button("button 3");
-		flowPanel_3.add(btnButton);
-		
-	
-		fireApparatusList = new FireApparatusList();		
+				
+		fireApparatusList = new ArrayList<FireApparatus>();
 		
 		RPC.apparatusService.loadApparatus(new AsyncCallback<ArrayList<FireApparatus>>() {
 			@Override
 			public void onSuccess(ArrayList<FireApparatus> result) {						
 				if(result != null){
-						lblName_1.setText(result.get(0).getFireApparatusSpec().getName());
-						lblYear_1.setText(result.get(0).getFireApparatusSpec().getYear());
-						lblmake_1.setText(result.get(0).getFireApparatusSpec().getMake());
-						lblmodel_1.setText(result.get(0).getFireApparatusSpec().getModel());
-						lbltype_1.setText(result.get(0).getFireApparatusSpec().getType());	
-						lbldescription_1.setText(result.get(0).getFireApparatusSpec().getDescription());
+					fireApparatusList = result;
+					lblName_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getName());
+					lblYear_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getYear());
+					lblmake_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getMake());
+					lblmodel_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getModel());
+					lbltype_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getType());	
+					lbldescription_1.setText(fireApparatusList.get(0).getFireApparatusSpec().getDescription());
+					
+					ScrollPanel scrollPanel = new ScrollPanel();
+					layoutPanel.add(scrollPanel);
+					layoutPanel.setWidgetLeftWidth(scrollPanel, 20.0, Unit.PX, 383.0, Unit.PX);
+					layoutPanel.setWidgetTopHeight(scrollPanel, 392.0, Unit.PX, 96.0, Unit.PX);
+					
+					FlowPanel flowPanel = new FlowPanel();
+					scrollPanel.setWidget(flowPanel);
+					flowPanel.setSize("100%", "100%");
+					
+					for(int i=0; i<fireApparatusList.size(); i++){
+						FlowPanel subFlowPanel_1 = new FlowPanel();
+						flowPanel.add(subFlowPanel_1);
+						
+//						Label lblPanel = new Label("panel" +Integer.toString(i));
+//						subFlowPanel_1.add(lblPanel);
+//						
+//						Button btnButton_2 = new Button("Button" +Integer.toString(i));
+//						subFlowPanel_1.add(btnButton_2);
+						
+						lblName01 = new Label("Name:");
+						subFlowPanel_1.add(lblName);
+						
+						lblName_2 = new Label("name" +Integer.toString(i));
+						subFlowPanel_1.add(lblName_2);						
+						lblName_2.setText(fireApparatusList.get(i).getFireApparatusSpec().getName());
+					}
+					
 				}
 				else{
 					lblName_1.setText("Fail");							
@@ -180,6 +174,49 @@ public class ApparatusView extends Composite implements ISubscriber {
 				lblName_1.setText("RPC failure");						
 			}
 		});
+		
+		
+		
+		
+		
+//		FlowPanel subFlowPanel_1 = new FlowPanel();
+//		flowPanel.add(subFlowPanel_1);
+//		
+//		Label lblPanel = new Label("panel 1");
+//		subFlowPanel_1.add(lblPanel);
+//		
+//		Button btnButton_2 = new Button("Button 1");
+//		subFlowPanel_1.add(btnButton_2);
+//		
+//		
+//		
+//		
+//		
+//		FlowPanel subFlowPanel_2 = new FlowPanel();
+//		flowPanel.add(subFlowPanel_2);
+//		
+//		Label lblPanel_1 = new Label("panel 2");
+//		subFlowPanel_2.add(lblPanel_1);
+//		
+//		Button btnButton_1 = new Button("Button 2");
+//		subFlowPanel_2.add(btnButton_1);
+//		
+//		
+//		
+//		
+//		FlowPanel subFlowPanel_3 = new FlowPanel();
+//		flowPanel.add(subFlowPanel_3);
+//		
+//		Label lblNewLabel = new Label("panel 2");
+//		subFlowPanel_3.add(lblNewLabel);
+//		
+//		Button btnButton = new Button("button 3");
+//		subFlowPanel_3.add(btnButton);
+		
+	
+		
+		
+		
 	}	
 
 	
@@ -188,3 +225,197 @@ public class ApparatusView extends Composite implements ISubscriber {
 		
 	}		
 }
+
+//
+//=======
+//package edu.ycp.CS320.client;
+//
+//import java.util.ArrayList;
+//
+//import com.google.gwt.dom.client.Style.Unit;
+//import com.google.gwt.event.dom.client.ClickEvent;
+//import com.google.gwt.event.dom.client.ClickHandler;
+//import com.google.gwt.user.client.rpc.AsyncCallback;
+//import com.google.gwt.user.client.ui.Button;
+//import com.google.gwt.user.client.ui.Composite;
+//import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+//import com.google.gwt.user.client.ui.InlineLabel;
+//import com.google.gwt.user.client.ui.Label;
+//import com.google.gwt.user.client.ui.LayoutPanel;
+//import com.google.gwt.user.client.ui.PasswordTextBox;
+//import com.google.gwt.user.client.ui.TextBox;
+//
+//import edu.ycp.CS320.shared.*;
+//import com.google.gwt.user.client.ui.Image;
+//import com.google.gwt.user.client.ui.ScrollPanel;
+//import com.google.gwt.user.client.ui.FlowPanel;
+//
+////mws
+//
+//public class ApparatusView extends Composite implements ISubscriber {
+//	private Label lblEquipmentHeader;
+//	private LayoutPanel layoutPanel;
+//	private Label lblName;
+//	private Label lblTitle;
+//	private FireApparatusList fireApparatusList;
+//	private Label lblType;
+//	private Label lblModel;
+//	private Label lblMake;
+//	private Label lblmake_1;
+//	private Label lblName_1;
+//	private Label lblmodel_1;
+//	private Label lbltype_1;
+//	private Label lbldescription_1;
+//	private Label lblDescription;
+//	private Label lblYear;
+//	private Label lblYear_1;
+//	
+//	
+//	public ApparatusView() {			
+//		layoutPanel = new LayoutPanel();
+//		layoutPanel.setStyleName("fireStation-panelBackground");
+//		initWidget(layoutPanel);
+//		layoutPanel.setSize("1142px", "761px");
+//		
+//		lblTitle = new Label("Apparatus Page!!!");
+//		lblTitle.setStyleName("Title");
+//		lblTitle.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+//		layoutPanel.add(lblTitle);
+//		layoutPanel.setWidgetLeftWidth(lblTitle, 333.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblTitle, 17.0, Unit.PX, 25.0, Unit.PX);
+//		
+//		lblEquipmentHeader = new Label("Equipment");
+//		layoutPanel.add(lblEquipmentHeader);
+//		layoutPanel.setWidgetLeftWidth(lblEquipmentHeader, 20.0, Unit.PX, 79.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblEquipmentHeader, 74.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblName = new Label("Name:");
+//		layoutPanel.add(lblName);
+//		layoutPanel.setWidgetLeftWidth(lblName, 20.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblName, 98.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblName_1 = new Label("name_1");
+//		layoutPanel.add(lblName_1);
+//		layoutPanel.setWidgetLeftWidth(lblName_1, 107.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblName_1, 98.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblMake = new Label("Make:");
+//		layoutPanel.add(lblMake);
+//		layoutPanel.setWidgetLeftWidth(lblMake, 20.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblMake, 146.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblModel = new Label("Model:");
+//		layoutPanel.add(lblModel);
+//		layoutPanel.setWidgetLeftWidth(lblModel, 20.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblModel, 171.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblType = new Label("Type:");
+//		layoutPanel.add(lblType);
+//		layoutPanel.setWidgetLeftWidth(lblType, 20.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblType, 195.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblDescription = new Label("Description:");
+//		layoutPanel.add(lblDescription);
+//		layoutPanel.setWidgetLeftWidth(lblDescription, 20.0, Unit.PX, 79.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblDescription, 219.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblmake_1 = new Label("make_1");
+//		layoutPanel.add(lblmake_1);
+//		layoutPanel.setWidgetLeftWidth(lblmake_1, 107.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblmake_1, 146.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblmodel_1 = new Label("model_1");
+//		lblmodel_1.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+//		layoutPanel.add(lblmodel_1);
+//		layoutPanel.setWidgetLeftWidth(lblmodel_1, 107.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblmodel_1, 171.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lbltype_1 = new Label("type_1");
+//		layoutPanel.add(lbltype_1);
+//		layoutPanel.setWidgetLeftWidth(lbltype_1, 107.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lbltype_1, 195.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lbldescription_1 = new Label("Description_1");
+//		layoutPanel.add(lbldescription_1);
+//		layoutPanel.setWidgetLeftWidth(lbldescription_1, 107.0, Unit.PX, 193.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lbldescription_1, 219.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblYear = new Label("Year:");
+//		layoutPanel.add(lblYear);
+//		layoutPanel.setWidgetLeftWidth(lblYear, 20.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblYear, 122.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		lblYear_1 = new Label("year_1");
+//		layoutPanel.add(lblYear_1);
+//		layoutPanel.setWidgetLeftWidth(lblYear_1, 107.0, Unit.PX, 56.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(lblYear_1, 122.0, Unit.PX, 18.0, Unit.PX);
+//		
+//		ScrollPanel scrollPanel = new ScrollPanel();
+//		layoutPanel.add(scrollPanel);
+//		layoutPanel.setWidgetLeftWidth(scrollPanel, 20.0, Unit.PX, 383.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(scrollPanel, 392.0, Unit.PX, 96.0, Unit.PX);
+//		
+//		FlowPanel flowPanel = new FlowPanel();
+//		scrollPanel.setWidget(flowPanel);
+//		flowPanel.setSize("100%", "100%");
+//		
+//		FlowPanel flowPanel_1 = new FlowPanel();
+//		flowPanel.add(flowPanel_1);
+//		
+//		Label lblPanel = new Label("panel 1");
+//		flowPanel_1.add(lblPanel);
+//		
+//		Button btnButton_2 = new Button("Button 1");
+//		flowPanel_1.add(btnButton_2);
+//		
+//		FlowPanel flowPanel_2 = new FlowPanel();
+//		flowPanel.add(flowPanel_2);
+//		
+//		Label lblPanel_1 = new Label("panel 2");
+//		flowPanel_2.add(lblPanel_1);
+//		
+//		Button btnButton_1 = new Button("Button 2");
+//		flowPanel_2.add(btnButton_1);
+//		
+//		FlowPanel flowPanel_3 = new FlowPanel();
+//		flowPanel.add(flowPanel_3);
+//		
+//		Label lblNewLabel = new Label("panel 2");
+//		flowPanel_3.add(lblNewLabel);
+//		
+//		Button btnButton = new Button("button 3");
+//		flowPanel_3.add(btnButton);
+//		
+//	
+//		fireApparatusList = new FireApparatusList();		
+//		
+//		RPC.apparatusService.loadApparatus(new AsyncCallback<ArrayList<FireApparatus>>() {
+//			@Override
+//			public void onSuccess(ArrayList<FireApparatus> result) {						
+//				if(result != null){
+//						lblName_1.setText(result.get(0).getFireApparatusSpec().getName());
+//						lblYear_1.setText(result.get(0).getFireApparatusSpec().getYear());
+//						lblmake_1.setText(result.get(0).getFireApparatusSpec().getMake());
+//						lblmodel_1.setText(result.get(0).getFireApparatusSpec().getModel());
+//						lbltype_1.setText(result.get(0).getFireApparatusSpec().getType());	
+//						lbldescription_1.setText(result.get(0).getFireApparatusSpec().getDescription());
+//				}
+//				else{
+//					lblName_1.setText("Fail");							
+//				}
+//			}
+//			
+//			@Override
+//			public void onFailure(Throwable caught) {
+//				lblName_1.setText("RPC failure");						
+//			}
+//		});
+//	}	
+//
+//	
+//	@Override
+//	public void eventOccurred(Object key, IPublisher publisher, Object hint) {		
+//		
+//	}		
+//}
+//>>>>>>> refs/remotes/Jake/master
--- ycp-cs320-project-ui/src/edu/ycp/CS320/client/HomePageView.java
+++ ycp-cs320-project-ui/src/edu/ycp/CS320/client/HomePageView.java
@@ -4,15 +4,15 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.Button;
+
+
+
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import edu.ycp.CS320.shared.ContactInfo;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 
+import edu.ycp.CS320.shared.ContactInfo;
 
 
 
@@ -26,6 +26,8 @@ public class HomePageView extends Composite{
 	private ApparatusView apparatusView = new ApparatusView();
 	private Button btnCalendar;
 	private theCalendar cal = new theCalendar();
+	private ContactInfoView contact = new ContactInfoView();
+	private Button btnContact;
 	
 	public HomePageView(){
 
@@ -38,13 +40,15 @@ public class HomePageView extends Composite{
 
 
 		initWidget(layout);		
-		Label hi = new Label("HOMEPAGE...\nAdd Links and Features here");		
+		Label hi = new Label("Welcome To York Fire Station");		
 		layout.add(hi);
+		layout.setWidgetLeftWidth(hi, 0.0, Unit.PX, 637.0, Unit.PX);
+		layout.setWidgetTopHeight(hi, 0.0, Unit.PX, 542.0, Unit.PX);
 		
 
 		btnApparatus = new Button("Apparatus");
 		layout.add(btnApparatus);
-		layout.setWidgetLeftWidth(btnApparatus, 21.0, Unit.PX, 120.0, Unit.PX);
+		layout.setWidgetLeftWidth(btnApparatus, 21.0, Unit.PX, 81.0, Unit.PX);
 		layout.setWidgetTopHeight(btnApparatus, 84.0, Unit.PX, 30.0, Unit.PX);
 		
 		btnApparatus.addClickHandler(new ClickHandler() {
@@ -70,6 +74,16 @@ public class HomePageView extends Composite{
 		layout.setWidgetLeftWidth(btnCalendar, 21.0, Unit.PX, 81.0, Unit.PX);
 		layout.setWidgetTopHeight(btnCalendar, 48.0, Unit.PX, 30.0, Unit.PX);
 		
+		btnContact = new Button("Contacts");
+		layout.add(btnContact);
+		layout.setWidgetLeftWidth(btnContact, 21.0, Unit.PX, 81.0, Unit.PX);
+		layout.setWidgetTopHeight(btnContact, 120.0, Unit.PX, 30.0, Unit.PX);
+		
+		Button btnVolunteer = new Button("Volunteer");
+		layout.add(btnVolunteer);
+		layout.setWidgetLeftWidth(btnVolunteer, 21.0, Unit.PX, 81.0, Unit.PX);
+		layout.setWidgetTopHeight(btnVolunteer, 156.0, Unit.PX, 30.0, Unit.PX);
+		
 		btnCalendar.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
@@ -77,5 +91,91 @@ public class HomePageView extends Composite{
 				layout.add(cal);				
 			}			
 		});		
+		
+		
+		btnContact.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				layout.clear();
+				layout.add(btnContact);
+			}
+		});
+		
+//
+//		=======
+//import com.google.gwt.dom.client.Style.Unit;
+//import com.google.gwt.event.dom.client.ClickEvent;
+//import com.google.gwt.event.dom.client.ClickHandler;
+//
+//
+//
+//
+///**
+// * @author drew
+// *this is the homepage which the logged in user will see after clicking log in
+// *right now it is lonely and needs features
+// */
+//public class HomePageView extends Composite{
+//	private Button btnApparatus;
+//	private ApparatusView apparatusView = new ApparatusView();
+//	private Button btnCalendar;
+//	private theCalendar cal = new theCalendar();
+//	
+//	public HomePageView(){
+//
+//		final LayoutPanel layout = new LayoutPanel();
+//		
+////		initWidget(layout);
+////		Label hi = new Label("HOMEPAGE...\nAdd Links and Features here");
+//
+////		LayoutPanel layout = new LayoutPanel();
+//
+//
+//		initWidget(layout);		
+//		Label hi = new Label("HOMEPAGE...\nAdd Links and Features here");		
+//		layout.add(hi);
+//		
+//
+//		btnApparatus = new Button("Apparatus");
+//		layout.add(btnApparatus);
+//		layout.setWidgetLeftWidth(btnApparatus, 21.0, Unit.PX, 120.0, Unit.PX);
+//		layout.setWidgetTopHeight(btnApparatus, 84.0, Unit.PX, 30.0, Unit.PX);
+//	
+//		
+//		
+//		
+//		
+//		
+//		btnApparatus.addClickHandler(new ClickHandler() {
+//			/* (non-Javadoc)
+//			 * @see com.google.gwt.event.dom.client.ClickHandler#onClick(com.google.gwt.event.dom.client.ClickEvent)
+//			 * 
+//			 * this method creates a local User object this is used to login to the system
+//			 * an async callback is created that will confirm if the RPC was successful or not
+//			 * 
+//			 */
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				System.out.println("Apparatus!");
+//				layout.clear();
+//				layout.add(apparatusView);				
+//			}		
+//			
+//		});
+//		
+//		
+//		btnCalendar = new Button("Calendar");
+//		layout.add(btnCalendar);
+//		layout.setWidgetLeftWidth(btnCalendar, 21.0, Unit.PX, 81.0, Unit.PX);
+//		layout.setWidgetTopHeight(btnCalendar, 48.0, Unit.PX, 30.0, Unit.PX);
+//		
+//		btnCalendar.addClickHandler(new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				layout.clear();
+//				layout.add(cal);				
+//			}			
+//		});		
+//>>>>>>> refs/remotes/Jake/master
 	}
 }
